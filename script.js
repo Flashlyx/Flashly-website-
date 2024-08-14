c06a35f26af89df5996ef15d193af75f2bd7385d
Flashlyx <146119156+Flashlyx@users.noreply.github.com>
2024-08-14 14:39:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,31 +5,3 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
-
-const form = document.querySelector('form');
-const FullName = document.getElementById("Fullname")
-const Email = document.getElementById("E-Mail")
-const Subject = document.getElementById("Subject")
-const Message = document.getElementById("Your Message")
-
-
-function sendEmail() {
-    const bodyMessage = 'FullName: ${fullName.value},<br>' Email: ${email.value}
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "61EA8ADB1BD5C6809E6F37B64CA34216027D",
-        Password : "password",
-        To : 'flashlytech@gmail.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    sendEmail();
-})
